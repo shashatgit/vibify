cd7ad955f122a46189e811a93bc33eb4f6cf5c41
shashatgit <137678630+shashatgit@users.noreply.github.com>
2025-07-01 00:28:59 +0530

    minor route changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { clerkMiddleware } from '@clerk/express'
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import adminRoutes from "./routes/admin.route.js"
@@ -16,6 +17,8 @@ const app = express();
 const PORT = process.env.PORT
 
 app.use(express.json());
+
+app.use(clerkMiddleware());
 
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import { protectRoute } from "../middleware/auth.middleware";
 
 const router = Router();
 
 router.get('/', (req,res) => {
+    req.auth.userId
     res.send("User with GET method...");
 })
 
